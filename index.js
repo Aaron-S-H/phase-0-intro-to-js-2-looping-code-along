80ab36e3c886dbe52a0eb2987edd8520067de040
Aaron Hemphill <hemphill.aaron@gmail.com>
2023-05-02 16:29:06 +0200

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solutions in this file
+
+
+
+function writeCards(names, event) {
+    const greetedNames = [];
+    for(let i=0; i< names.length; i++){
+        console.log(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+        greetedNames.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+}
+return greetedNames;
+}
+writeCards(["Guadalupe", "Ollie", "Aki"], 'surprise');
+
+
+function countDown(number) {
+    while (number >=0) {
+        console.log(number);
+        number --;
+    }
+    return number;
+}
+countDown(10);
